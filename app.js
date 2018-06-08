7f745c3b800917f214741f20fce9cacce58da297
Angel Juarbe <angeljuarbe@Angels-MBP.fios-router.home>
2018-06-07 21:40:04 -0400

    Added Post route

--- app.js
+++ app.js
@@ -3,8 +3,23 @@ const app = express();
 const bodyParser = require('body-parser');
 const port =  process.env.PORT || 5000;
 
+const companies = [
+  {
+    name: 'SmartBear Software',
+    image: 'https://smartbear.com/SmartBear/media/images/smartbear-color-logo-s.png'
+  },
+  {
+    name: 'Wayfair',
+    image: 'https://d2xsegqaa8s978.cloudfront.net/wayfair_0.0.4_staging/assets/logo.png'
+  },
+  {
+    name: 'Akamai Technologies',
+    image: 'https://www.akamai.com/us/en/multimedia/images/logo/akamai-logo.png'
+  }
+];
+
 app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded( { extended: true }))
+app.use(bodyParser.urlencoded( { extended: true }));
 app.use('/css', express.static('css'));
 
 app.get('/', (req, res) => {
@@ -12,27 +27,19 @@ app.get('/', (req, res) => {
 });
 
 app.get('/companies', (req, res) => {
-  const companies = [
-    {
-      name: 'SmartBear Software',
-      image: 'https://smartbear.com/SmartBear/media/images/smartbear-color-logo-s.png'
-    },
-    {
-      name: 'Wayfair',
-      image: 'https://d2xsegqaa8s978.cloudfront.net/wayfair_0.0.4_staging/assets/logo.png'
-    },
-    {
-      name: 'Akamai Technologies',
-      image: 'https://www.akamai.com/us/en/multimedia/images/logo/akamai-logo.png'
-    }
-  ];
   res.render('companies', { companies: companies });
 });
 
-app.get('companies/new', (req, res) => {
-  res.render('newCompany', { });
+app.get('/companies/new', (req, res) => {
+  res.render('newCompany', {  });
 });
 
+app.post('/companies', (req, res) => {
+  // let name = req.params.name;
+  // let image = req.params.image;
+  companies.push({name:req.body.name, image: req.body.image});
+  res.redirect('/companies');
+});
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}...`);
